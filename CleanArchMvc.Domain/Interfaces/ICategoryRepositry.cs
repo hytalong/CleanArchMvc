e99f108fd2289bff39804f42daf0a4aefeab2b74
Hytalo P <is.oscar@hotmail.com>
2026-02-25 00:32:12 -0300

    Criando as Interfaces dos Repositorios

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepositry.cs
@@ -0,0 +1,17 @@
+﻿
+using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepositry
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<Product>> GetById(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(int id);
+    }
+}
