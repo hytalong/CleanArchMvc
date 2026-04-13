03b4022006c532bc637fd9fe9cf52843447cbdb9
Hytalo P <is.oscar@hotmail.com>
2026-04-13 18:13:23 -0300

    Implementando o serviço para Product - ProductService

---
+++ CleanArchMvc.Application/Interface/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interface
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetById(int? id);
+
+        Task<ProductDTO> GetProductCategory(int? id);
+        Task Add(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
+    }
+}
