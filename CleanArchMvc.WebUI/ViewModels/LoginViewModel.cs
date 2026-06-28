ac69d92245a2f139a6f0bb5f1965fadfab2d2bde
Hytalo P <is.oscar@hotmail.com>
2026-06-28 15:09:03 -0300

    Implementando o Login, Register e Logout em AccountRegisterController

---
+++ CleanArchMvc.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(20, ErrorMessage = "Password must be at least {2} and at max " +
+        "{1} characters long.", MinimumLength = 10)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public string ReturnUrl { get; set; }
+}
