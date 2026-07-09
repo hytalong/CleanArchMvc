4d90544a63b98d3a366660fcab6ea69471267401
Hytalo P <is.oscar@hotmail.com>
2026-07-09 11:56:01 -0300

    Migrando as funcionalidades da Classe Startup para a Program

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -1,26 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
+using CleanArchMvc.IoC;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace CleanArchMvc.API
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+
+builder.Services.AddInfrastructureAPI(builder.Configuration);
+//ativar autrenticação e validar o token
+builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStatusCodePages();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,19 +1,51 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.IoC;
 
-namespace CleanArchMvc.WebUI;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//seedUserRoleInitial.SeedRoles();
+//seedUserRoleInitial.SeedUsers();
+SeedUserRole(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void SeedUserRole(IApplicationBuilder app)
+{
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+        var seed = serviceScope.ServiceProvider;
+        var seedUserRoleInitial = seed
+            .GetRequiredService<ISeedUserRoleInitial>();
+        seedUserRoleInitial.SeedRoles();
+        seedUserRoleInitial.SeedUsers();
+    }
 }
